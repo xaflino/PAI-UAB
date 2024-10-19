1f59079045748c77fd2606b1148352d39f6e8b23
xaflino <xavilopezgonzalez21@gmail.com>
2024-10-19 18:46:55 +0200

    Exercici 13.1

---
+++ condicionals.js
@@ -0,0 +1,6 @@
+// 13.1.- Escriu un programa que donat un número llegit amb prompt escrigui el missatge "el número llegit és negatiu" si el número és més petit que zero.
+let numeroNegatiu = parseInt(prompt("Introdueix un número:"));
+if (numeroNegatiu<0) {
+    document.write("El numero llegit es negatiu");
+    console.log("El numero llegit es negatiu");
+}
