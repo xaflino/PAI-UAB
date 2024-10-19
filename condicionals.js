8076021ebbf52166d640e1db1464b780e62d891c
xaflino <xavilopezgonzalez21@gmail.com>
2024-10-19 19:06:30 +0200

    Update condicionals.js

--- condicionals.js
+++ condicionals.js
@@ -1,6 +1,17 @@
 // 13.1.- Escriu un programa que donat un número llegit amb prompt escrigui el missatge "el número llegit és negatiu" si el número és més petit que zero.
-let numeroNegatiu = parseInt(prompt("Introdueix un número:"));
-if (numeroNegatiu<0) {
-    document.write("El numero llegit es negatiu");
-    console.log("El numero llegit es negatiu");
-}
+//   let numeroNegatiu = parseInt(prompt("Introdueix un número:"));
+//   if (numeroNegatiu<0) {
+//      document.write("El numero llegit es negatiu");
+//      console.log("El numero llegit es negatiu");
+//  }
+
+// 14.2.- Escriu un programa que donat un número llegit amb prompt, escrigui el missatge “el número llegit és parell” si el número es parell i “el número llegit es senar” en cas contrari.
+
+//  let nombre = prompt("introdueix nombre");   //Porque no parseint
+//  if (nombre % 2 == 0) {
+//     document.write("El nombre es parell");
+//     console.log("El nombre es parell");
+//  } else {
+//     document.write("El nombre es senar");
+//     console.log("El nombre es senar");
+//  }
