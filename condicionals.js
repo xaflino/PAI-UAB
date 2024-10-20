7d86fe540dc4930917275adb747daa9b2862bda6
xaflino <xavilopezgonzalez21@gmail.com>
2024-10-20 17:59:59 +0200

    Update condicionals.js

--- condicionals.js
+++ condicionals.js
@@ -18,11 +18,27 @@
 
 // 14.3.- Escriu un programa que, donat dos números llegits amb prompt, escrigui a la consola quin és el més gran. Només pots fer servir una instrucció if-else.
 
-let numero1=parseInt(prompt("Escriu numero"));
-let numero2=parseInt(prompt("Escriu numero"));
+// let numero1=parseInt(prompt("Escriu numero"));
+// let numero2=parseInt(prompt("Escriu numero"));
 
-if (numero1>numero2){
-    console.log("El nombre més gran es el primer:"+numero1)
-} else {
-    console.log("El nombre més gran es el segon:"+numero2)
+// if (numero1>numero2){
+//     console.log("El nombre més gran es el primer:"+numero1)
+// } else {
+//     console.log("El nombre més gran es el segon:"+numero2)
+// }
+
+// 15.3.- Escriu un programa, que donat una lletra que representa un dia de la setmana (l - dilluns, m - dimarts, x - dimecres, j - dijous, ...) escrigui a la consola si el dia és laborable o festiu. Són laborables de dilluns a divendres. Fes servir la instrucció switch.
+
+let dia=prompt("Escriu la primera lletra del dia de la setmana que vulguis en minúcula:");
+
+switch (dia){
+    case "l":
+        console.log("Laborable");
+    case "m":
+        console.log("Laborable");
+    case "x":
+        console.log("Laborable");
+    case "s":
+        console.log("Festivo")
+
 }
