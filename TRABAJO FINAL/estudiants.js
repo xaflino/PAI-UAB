9ffcef6c1a0c0df39700671b441857bbd36e4b3e
xaflino <xavilopezgonzalez21@gmail.com>
2024-12-17 13:31:32 +0100

    Hola

---
+++ TRABAJO FINAL/estudiants.js
@@ -0,0 +1,28 @@
+// Treballarem sempre amb una variable global, obj, què és una taula on estan 
+// guardats tots els accidents de l'any. Qualsevol altre variable que necessitem
+// haurà de ser, necessàriament, una variable local.
+
+// Com que al document html no hi ha controladors d'esdeveniments, els haurem de crear aquí:
+
+
+
+// Teniu ja definides les funcions de cada exercici (menys del cinquè), només cal
+// que ompliu el codi necessari.
+
+function exercici01() {
+
+}
+
+function exercici02() {
+
+}
+
+
+function exercici03() {
+ 
+}
+
+
+function exercici04() {
+    creaFormulari();
+}
