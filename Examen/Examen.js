7dee6aefdb5013935b0770fe5be941f4d0b62650
xaflino <xavilopezgonzalez21@gmail.com>
2025-01-04 13:31:00 +0100

    Hola

--- Examen/Examen.js
+++ Examen/Examen.js
@@ -1,6 +1,33 @@
 const nom = document.getElementById("nom");
 const cognom = document.getElementById("cognom");
 
-function capitalitzarNom(text){
+// Habilitar Sexe
+document.getElementById("nom").addEventListener("blur", function(){
+    if (nom.value!==""){
+        cognom.disabled=false;
+    }
+    else{
+        cognom.disabled=true;
+    }
     
+})
+
+
+
+
+
+
+
+// Estils
+nom.onfocus=function(){
+    nom.style.color="black";
+}
+nom.onblur=function(){
+    nom.style.color="gray";
+}
+cognom.onfocus=function(){
+    cognom.style.color="black";
+}
+cognom.onblur=function(){
+    cognom.style.color="gray";
 }
