6131d5467c6bd88c97f2a09b8e5cd06ea9658d7b
xaflino <xavilopezgonzalez21@gmail.com>
2025-01-05 19:49:58 +0100

    Hola

--- Examen/Examen.js
+++ Examen/Examen.js
@@ -1,7 +1,7 @@
 const nom = document.getElementById("nom");
 const cognom = document.getElementById("cognom");
 
-// Habilitar Sexe
+// Habilitar Cognom
 document.getElementById("nom").addEventListener("blur", function(){
     if (nom.value!==""){
         cognom.disabled=false;
@@ -12,11 +12,47 @@ document.getElementById("nom").addEventListener("blur", function(){
     
 })
 
+// Habilitar Sexe
+cognom.addEventListener("blur",function(){
+    if (cognom.disabled==false){
+        document.getElementById("sexe").disabled=false
+    }
+})
 
+// Habilitar nivell
+document.getElementById("sexe").addEventListener("blur",function(){
+    if (document.getElementById("sexe").disabled==false){
+        document.getElementById("nivell").disabled=false;
+    }
+})
 
+// Habiltar entreno
+document.getElementById("nivell").addEventListener("blur",function(){
+    if (document.getElementById("nivell").disabled==false){
+        document.getElementById("si").disabled=false;
+        document.getElementById("no").disabled=false;
+    }
+})
 
+// Habilitar On i Quant
+document.getElementById("si").addEventListener("change", function() {
+    if (this.checked) {
+        document.getElementById("on").disabled = false;
+        document.getElementById("un").disabled = false;
+        document.getElementById("dos").disabled = false;
+        document.getElementById("més").disabled = false;
+    }
+});
 
-
+document.getElementById("no").addEventListener("change", function() {
+    if (this.checked) {
+        document.getElementById("on").disabled = true;
+        document.getElementById("un").disabled = true;
+        document.getElementById("dos").disabled = true;
+        document.getElementById("més").disabled = true;
+    }
+});
+    
 
 // Estils
 nom.onfocus=function(){
