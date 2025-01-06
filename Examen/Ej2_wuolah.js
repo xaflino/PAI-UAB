92261b7048f32a79d0142ec8debae10f96105591
xaflino <xavilopezgonzalez21@gmail.com>
2025-01-06 20:49:50 +0100

    hh

---
+++ Examen/Ej2_wuolah.js
@@ -0,0 +1,24 @@
+function comprova(){
+    let contraseña=document.getElementById("pas1").value;
+    let numeros="1234567890"
+    let digits = false
+    if(contraseña.length<10){
+        document.getElementById("resultat").innerHTML="La contrasenya ha de tenir almenys 10 caràcters";
+        return
+    } 
+
+    for(let i=0;i<contraseña.length;i++){
+        for(let j=0;j<numeros.length;j++){
+            if(contraseña[i]===numeros[j]){
+                digits=true;
+                break;
+            } 
+        }
+        if (digits) break;
+    }
+    if (digits===false){
+        document.getElementById("resultat").innerHTML="La contrasenya ha de tenir almenys un digit"; 
+    } else{
+        document.getElementById("resultat").innerHTML="La contrasenya compleix els requisits";
+    }
+}
